4e7526017d0a9ef300593aaf492c749bd708136e
killer933 <china.sunzhi@gmail.com>
2017-11-13 10:13:01 +0800

    Add files via upload

--- sh/testHouseInfo.sh
+++ sh/testHouseInfo.sh
@@ -2,6 +2,6 @@
 cd /root/myscripts/py/png/
 /usr/local/bin/python3.6 /root/myscripts/py/testHouseInfoBar.py
 sleep 5m
-echo " " | mailx  -s "houseInfo Bar`date +%F`" -a /root/myscripts/py/png/new.png -a /root/myscripts/py/png/old.png 102044977@qq.com
-echo " " | mailx  -s "houseInfo Bar`date +%F`" -a /root/myscripts/py/png/new.png -a /root/myscripts/py/png/old.png sunzhi1019@163.com
+echo " " | mailx  -s "houseInfo Bar`date +%F`" -a /root/myscripts/py/png/new.png -a /root/myscripts/py/png/old.png XXXXX@qq.com
+echo " " | mailx  -s "houseInfo Bar`date +%F`" -a /root/myscripts/py/png/new.png -a /root/myscripts/py/png/old.png XXXXX@163.com
 exit
--- sh/testSpider.sh
+++ sh/testSpider.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /root/testSpider/tutorial
 scrapy crawl MySpider -o house.csv
-echo " " | mailx  -s "house information `date +%F`" -a /root/testSpider/tutorial/house.csv sunzhi1019@163.com
+echo " " | mailx  -s "house information `date +%F`" -a /root/testSpider/tutorial/house.csv XXXXXXX@163.com
 exit
